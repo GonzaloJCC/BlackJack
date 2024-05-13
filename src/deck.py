499a276de3fc3e0ea300d82d41e56cb7b189a2b2
GonzaloJCC <gonzalojimenezcarretero@gmail.com>
2024-05-13 17:37:40 +0200

    Added gitignore, created card class and started deck class

---
+++ src/deck.py
@@ -0,0 +1,44 @@
+import random
+
+
+
+#class card
+class Card:
+    def __init__(self, value, suit):
+        self.value = value
+        self.suit = suit
+
+    def __str__(self):
+        return f"{self.value}{self.suit}"
+
+
+#class deck, stores 52 different cards and has different methods required to play games
+class Deck:
+    def __init__(self):
+        suits = ["♧", "♤", "♥", "♦"]
+        values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+
+        self.deck = []  #the deck of cards
+
+        for suit in suits:
+            for value in values:
+                card = Card(value, suit)
+                self.deck.append(card)
+        
+    def shuffle(self):
+        pass
+    def deal(self):
+
+        pass
+
+
+deck = Deck()
+count = 0
+for i in deck.deck:
+    print(i)
+    count+=1
+
+print(count)
+
+
+
