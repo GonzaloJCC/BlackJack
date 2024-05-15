5d996b830acdaec8eba9f1d70bc66b8442cd707e
GonzaloJCC <gonzalojimenezcarretero@gmail.com>
2024-05-16 00:05:42 +0200

    Created game loop and some minor modifications

--- src/blackjack.py
+++ src/blackjack.py
@@ -1,13 +1,29 @@
-from deck import *
-
-def test():
-    deck = Deck()
-    count = 0
-    for i in deck.deck:
-        print(i)
-        count+=1
-
-    print(count)
+from person import *
+class Table:
+    def __init__(self):
+        pass
 
 
-test()
+
+
+#main
+playerAmmount = 0
+players = []
+bets = [4]
+
+while(playerAmmount < 1 or playerAmmount > 4):
+    playerAmmount = int(input("Enter the ammount of players(1-4): "))
+    
+
+for i in range (0, playerAmmount):
+    name = input(f"Enter the player {i+1} name: ")
+    players.append(Player(name))
+    bets.append(0)
+
+
+while(1):
+    print("PLACE YOUR BETS")
+    for i in range(0, playerAmmount):
+        
+        bet = int(input(f"PLAYER {i+1}, bet: "))
+        bets[i] = (players[i].Bet(bet))
--- src/person.py
+++ src/person.py
@@ -1,11 +1,29 @@
 STARTING_CHIPS = 1000
 
+#score dealer must stop (all 17)
+DEALER_STOP = 17
+
+#actions
+HIT = "Hit"
+DOUBLE = "Double"
+SPLIT = "Split"
+STAND = "Stand"
 
 
 class Player:
-    def __init__(self):
+    def __init__(self, name):
         self.hand = []
         self.chips = STARTING_CHIPS
+        self.name = name
+
+    def Bet(self, ammount):
+        while(ammount > self.chips or ammount < 1):
+            print("You don't have enough chips to bet, enter a valid number")
+            ammount = int(input("Bet: "))
+
+        self.chips-ammount
+        return ammount
+
 
 
 class Dealer:
