7c89ad4d1ede01415aa2a33b6bc232e2083ef60b
GonzaloJCC <gonzalojimenezcarretero@gmail.com>
2025-05-12 20:32:42 +0200

    Added a 'menu', chips given when split needs to be fixed

--- src/main.py
+++ src/main.py
@@ -1,8 +1,46 @@
 from blackjack import BlackJack
+from blackjack import cls
+
 
 #TODO - make a menu BLACKJACK, press any key to start
     # input("PRESS ANY KEY TO START: ")
+
+
+def game_rules():
+    
+    cls()
+    print("GAME RULES:\n")
+    print("\t-> Dealer must stand on all 17")
+    print("\t-> A split after another split is not allowed")
+    print("\t-> A double after a split is not allowed")
+    print("\t-> The game is played with 6 decks, every 15 rounds the decks will be swapped with" \
+    "\n\t   6 new ones")
+    print("\t-> A win with blackjack will multiply the bet by 2.5")
+    print("\t-> A win withoyt blackjack will multyply the bet by 2")
+    print("\n\n\n")
+    x = input("Type anything to exit this menu: ")
+    cls()
+
 def main() -> None:
+    cls()
+    while True:
+        print("WELCOME!")
+        print("IF YOU WANT TO KNOW THE RULES TYPE 'R'")
+        print("IF YOU WANT TO PLAY TYPE 'P'")
+        decision = input("> ").upper()
+        
+        if decision not in ['R', 'P']:
+            print("YOU MUST TYPE A VALID INPUT\n")
+            continue
+
+        if decision == 'P':
+            break
+
+        if decision == 'R':
+            game_rules()
+
+
+
     #cambiar constructor para recibir player
     #almacenar datos del usuario, funcion de crear cuenta etc
     bj = BlackJack()
