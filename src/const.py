ea53054e4facc73f0ba14f47b5f334e29392b13e
GonzaloJCC <gonzalojimenezcarretero@gmail.com>
2025-07-05 17:32:00 +0200

    New functions to write text added

--- src/const.py
+++ src/const.py
@@ -1,3 +1,4 @@
+# Default variables
 STARTING_CHIPS = 1000
 NUMBER_OF_DECKS = 6
 RESET_DECK_COUNT = 15
@@ -8,9 +9,6 @@ OBJECTIVE = 21
 BUSTED = -1
 
 # The constant your bets multiply by if you win
-# BLACKJACK_WIN_RATIO = 2.5
-# WIN_RATIO = 2
-
 BLACKJACK_WIN_RATIO = 1.5
 WIN_RATIO = 1
 
@@ -25,15 +23,46 @@ SPEED_FAST = 1.5
 SPEED_ULTRA_FAST = 2
 
 
-
-# GUI
-# from screeninfo import get_monitors
-# monitor = get_monitors()[0]
-# HEIGHT = monitor.height
-# WIDTH= monitor.width
+#######
+# GUI #
+#######
 
 WIDTH = 1920
 HEIGHT = 1030
 FPS = 60
 
 TITLE = "BlackJack"
+
+# Color options
+COLOR_BLACK   = (0, 0, 0)
+COLOR_WHITE   = (255, 255, 255)
+COLOR_RED     = (255, 0, 0)
+COLOR_GREEN   = (0, 255, 0)
+COLOR_BLUE    = (0, 0, 255)
+COLOR_YELLOW  = (255, 255, 0)
+COLOR_CYAN    = (0, 255, 255)
+COLOR_MAGENTA = (255, 0, 255)
+COLOR_GRAY    = (128, 128, 128)
+COLOR_DARK_GRAY = (64, 64, 64)
+COLOR_LIGHT_GRAY = (192, 192, 192)
+COLOR_ORANGE  = (255, 165, 0)
+COLOR_PURPLE  = (128, 0, 128)
+COLOR_BROWN   = (139, 69, 19)
+COLOR_PINK    = (255, 192, 203)
+COLOR_LIME    = (0, 255, 0)
+COLOR_NAVY    = (0, 0, 128)
+COLOR_GOLD    = (255, 215, 0)
+
+# Font options
+FONT_ARIAL = 'arial'
+FONT_ARIAL_BLACK = 'arialblack'
+FONT_COMIC_SANS = 'comicsansms'
+FONT_COURIER_NEW = 'couriernew'
+FONT_VERDANA = 'verdana'
+FONT_IMPACT = 'impact'
+FONT_TIMES_NEW_ROMAN = 'timesnewroman'
+FONT_GEORGIA = 'georgia'
+FONT_TREBUCHET = 'trebuchetms'
+FONT_LUCIDA_CONSOLE = 'lucidaconsole'
+FONT_TAHOMA = 'tahoma'
+FONT_CALIBRI = 'calibri'
--- src/graphics.py
+++ src/graphics.py
@@ -2,9 +2,10 @@ from src.const import *
 import pygame
 import sys
 
+
+# Function in charge of the graphic section
 def start_gui():
     pygame.init()
-    # cls()
     pygame.display.set_caption(TITLE)
     icon = pygame.image.load("./assets/icon/icon.png")
     pygame.display.set_icon(icon)
@@ -13,12 +14,37 @@ def start_gui():
 
     clock = pygame.time.Clock()
 
-    while True:
+    RUNNING = True
+    while RUNNING:
+
+        screen.fill(COLOR_DARK_GRAY)
+
+        draw_text(500, 500, screen, "Hello World!", FONT_VERDANA, 40)
+
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+                RUNNING = False
 
         pygame.display.update()
         clock.tick(FPS)
     pygame.quit()
+    sys.exit()
+
+# Function to choose the font to use
+def use_font(font_name: str=None, font_size: int=40, bold: bool=False, italic:bool =False):
+    return pygame.font.SysFont(font_name, font_size, bold, italic)
+
+# Function used to draw a text
+def draw_text(pos_x, pos_y, screen, text, font_name, font_size, text_color=COLOR_WHITE):
+    img = use_font(font_name, font_size).render(text, True, text_color)
+    screen.blit(img, (pos_x, pos_y))
+
+# Menu screen 
+def menu():
+    pass
+
+
+# Similar to blackjack.py's gameloop
+def play():
+    pass
